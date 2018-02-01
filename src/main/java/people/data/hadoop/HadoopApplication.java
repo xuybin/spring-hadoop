38889c20d2c99ac31eb5c5c4256e58b71e89eff8
Xu YongBin <xuybin@qq.com>
2018-02-01 16:20:59 +0800

    first commit

---
+++ src/main/java/people/data/hadoop/HadoopApplication.java
@@ -0,0 +1,12 @@
+package people.data.hadoop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HadoopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HadoopApplication.class, args);
+	}
+}
