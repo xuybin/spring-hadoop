664213908a22ea98539f600d20cc71a883515681
Xu YongBin <xuybin@qq.com>
2018-02-03 16:45:50 +0800

    Spring Boot2.0+Spring WebFlux+Spring Data Hadoop

--- src/main/java/people/data/hadoop/Application.java
+++ src/main/java/people/data/hadoop/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-@ImportResource("classpath:hbase-spring.xml")
+@ImportResource("classpath:hadoop-spring.xml")
 public class Application {
 
 	public static void main(String[] args) {
